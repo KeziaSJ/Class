29f6c723f905790c7e49a5474f99b1a5d96b7645
shilpaalex <shilpa@deerom.com>
2021-02-11 11:18:27 +0530

    modified

--- vehicle.py
+++ vehicle.py
@@ -4,4 +4,4 @@ class Vehicle:
         self.mileage = mileage
 
 modelX = Vehicle(240, 18)
-print(modelX.max_speed, modelX.mileage)
+print("maximum speed of vehicle is",modelX.max_speed,"with mileage", modelX.mileage)
