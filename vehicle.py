0ada0ffa2cbca06186e6fcfe5bb9d8344e6d76fc
KeziaSJ <kezia@deerom.com>
2021-02-11 11:48:38 +0530

    again modified

--- vehicle.py
+++ vehicle.py
@@ -1,7 +1,11 @@
 class Vehicle:
-    def __init__(self, max_speed, mileage):
+
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
         self.max_speed = max_speed
         self.mileage = mileage
+class Bus(Vehicle):
+    pass
+School_bus = Bus("School Volvo", 180, 12)
+print("Vehicle Name:", School_bus.name, "Speed:", School_bus.max_speed, "Mileage:", School_bus.mileage)
 
-modelX = Vehicle(240, 18)
-print("maximum speed of vehicle is",modelX.max_speed,"with mileage", modelX.mileage)
